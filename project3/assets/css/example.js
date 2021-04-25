40f2d8b4da96bc8a9a043da13e68f6489ac5f23b
sc1343 <47128692+sc1343@users.noreply.github.com>
2021-04-24 20:30:46 -0400

    Add files via upload

---
+++ project3/assets/css/example.js
@@ -0,0 +1,40 @@
+function myFunction() {
+  var x = document.getElementById("myImg").alt;
+  document.getElementById("demo").innerHTML = x;
+}
+
+function myFunction2() {
+  var x = document.getElementById("myImg2").alt;
+  document.getElementById("demo2").innerHTML = x;
+}
+function myFunction3() {
+  var x = document.getElementById("myImg3").alt;
+  document.getElementById("demo3").innerHTML = x;
+}
+
+function myFunction4() {
+  var x = document.getElementById("myImg4").alt;
+  document.getElementById("demo4").innerHTML = x;
+}
+
+function myFunction5() {
+  var x = document.getElementById("myImg5").alt;
+  document.getElementById("demo5").innerHTML = x;
+}
+function myFunction6() {
+  var x = document.getElementById("myImg6").alt;
+  document.getElementById("demo6").innerHTML = x;
+}
+
+function myFunction7() {
+  var x = document.getElementById("myImg7").alt;
+  document.getElementById("demo7").innerHTML = x;
+}
+function myFunction8() {
+  var x = document.getElementById("myImg8").alt;
+  document.getElementById("demo8").innerHTML = x;
+}
+function myFunction9() {
+  var x = document.getElementById("myImg9").alt;
+  document.getElementById("demo9").innerHTML = x;
+}
